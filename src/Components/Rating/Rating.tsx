63fbb22858af4a7ebac0e98c2ad23e999970c468
rafael isac <rafael.darrigo@gmail.com>
2025-03-27 11:17:07 -0300

    alteration_space_skill

--- src/Components/Rating/Rating.tsx
+++ src/Components/Rating/Rating.tsx
@@ -14,8 +14,8 @@ export const Rating = ({ data }: Props) => {
   const stars = emojis[notaInt].repeat(notaInt) + emojis[0].repeat(5 - notaInt);
 
   return (
-    <div className="flex justify-center items-center text-xl w-72 hover:scale-110">
-      <div className="text-base  font-Montserrat text-mintcream">
+    <div className="flex flex-col gap-4  justify-center items-center text-xl w-72 hover:scale-110">
+      <div className="text-lg  font-Montserrat text-mintcream">
         {data.nameSkill}
       </div>
       <div className="ml-3">{stars}</div>

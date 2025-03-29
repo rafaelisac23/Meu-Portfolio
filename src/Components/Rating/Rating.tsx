5eaacfe94c60a49ca9e4c8b71bd727e3a2a4249f
rafael isac <rafael.darrigo@gmail.com>
2025-03-29 14:12:48 -0300

    Finished_complements

--- src/Components/Rating/Rating.tsx
+++ src/Components/Rating/Rating.tsx
@@ -14,11 +14,11 @@ export const Rating = ({ data }: Props) => {
   const stars = emojis[notaInt].repeat(notaInt) + emojis[0].repeat(5 - notaInt);
 
   return (
-    <div className="flex flex-col gap-4  justify-center items-center text-xl w-72 hover:scale-110">
-      <div className="text-lg  font-Montserrat text-mintcream">
+    <div className="flex flex-col  w-28  justify-center items-center text-xl hover:scale-110">
+      <div className="text-sm md:text-lg font-Montserrat text-mintcream ">
         {data.nameSkill}
       </div>
-      <div className="ml-3 text-white">{stars}</div>
+      <div className="text-white">{stars}</div>
     </div>
   );
 };
